e9f571c7f6ac81c31fd5f4cac63729f0088b605c
Destiny Tibbitts <dtibbs5145@gmail.com>
2020-01-17 22:08:57 -0500

    MVP HIT

--- Posts/post-router.js
+++ Posts/post-router.js
@@ -15,15 +15,15 @@ router.get('/', (req, res) => {
 
 //------GET BY ID-----
 router.get('/:id', (req, res) => {
-    db.findById(req.params.id)
+    db.findById(req.params.id) // requires the id input
         .then(db => {
             if(db) {
-                res.status(200).json(db)
+                res.status(200).json(db) //if that ID is in the DB, that post will be retrieved
             } else {
-                res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+                res.status(404).json({ message: 'The post with the specified ID does not exist.' }); //if there is no post with that ID, a 404(not found) will be returned
             }
         }) .catch(err => {
-            res.status(500).json({ message: 'The post information could not be retrieved.', err });
+            res.status(500).json({ message: 'The post information could not be retrieved.', err }); // if there was an error in the code, 500 will return
         });
 });
 
@@ -41,6 +41,72 @@ router.get('/:id/comments', (req, res) => {
         });
 });
 
+//----POST-----
+router.post('/', (req, res) => {
+    db.insert(req.body)
+        .then(post => {
+            if(post) {
+                res.status(201).json(post);
+            } else {
+                res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+            }
+        }) .catch(err => {
+            res.status(500).json({error: 'There was an error while saving the post to the database', err});
+        });
+});
+
+
+//-----POST COMMENTS-----
+router.post('/:id/comments', (req, res) => {
+    const commentInfo = req.body;
+    commentInfo.post_id = req.params.id;
+    db.insertComment(commentInfo)
+        .then(comment => {
+            if(comment) {
+                res.status(201).json(comment); //if everything is right, comment gets created
+            } else if(db) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist' }); //if something is wrong, then 404 will rendering if the ID doesn't exist
+            } else {
+                res.status(400).json({ errorMessage: 'Please provide text for the comment' }); //Text for the comment is required
+            }
+        }) .catch(err => {
+            res.status(500).json({ error: 'There was an error while saving the comment to the database' });
+        });
+});
+
+//-------PUT------- 
+//The update works when you double check, but initially it tells you it couldn't be modified
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    db.update(req.params.id, changes)
+        .then(db => {
+            if(post) {
+                res.status(200).json(post);
+            } else if (db) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            } else {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+            }
+        }) .catch(err => {
+            res.status(500).json({ error: "The post information could not be modified." });
+        });
+});
+
+
+//-------- DELETE ------
+//The post will be removed, but it will say it couldn't be deleted
+router.delete('/:id', (req, res) => {
+    db.remove(req.params.id)
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json({ message: `Deleted ${post_id} successfully` });
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist' });
+            }
+        }) .catch(err => {
+            res.status(500).json({ err: 'The post could not be removed' });
+        });
+});
 
 
 module.exports = router;
